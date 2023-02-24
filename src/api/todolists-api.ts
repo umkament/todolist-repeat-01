d6ff48b1a842f984efb3293d8a2d765fad5f5a23
umkament <97944243+umkament@users.noreply.github.com>
2023-02-24 21:40:08 +0300

    урок 13-01

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -6,13 +6,19 @@ const settings = {
     "API-KEY": "198377a1-b97d-41c4-a5b8-692b6bfcbc08"
   }
 }
+
+const instance = axios.create({
+  ...settings,
+  baseURL: 'https://social-network.samuraijs.com/api/1.1/'
+})
+
 type TodolistType = {
   id: string
   title: string
   addedDate: string
   order: number
 }
-type ResponseType<D>={
+type ResponseType<D={}>={
   resultCode: number
   messages: [],
   data: D
@@ -21,15 +27,28 @@ type ResponseType<D>={
 
 export let todolistsAPI = {
   getTodolists() {
-    return axios.get<Array<TodolistType>>('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+    const promise = instance.get<Array<TodolistType>>('todo-lists')
+    return promise
   },
   createTodolist(title: string) {
-    return axios.post<ResponseType<{item: TodolistType}>>('https://social-network.samuraijs.com/api/1.1/todo-lists', {title}, settings)
+    return instance.post<ResponseType<{item: TodolistType}>>('todo-lists', {title})
   },
   deleteTodolist(todolistId: string) {
-    return axios.delete<ResponseType<{}>>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, settings)
+    return instance.delete<ResponseType>(`todo-lists/${todolistId}`)
   },
   updateTodolist(todolistId: string, title: string) {
-    return axios.put<ResponseType<{}>>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, {title}, settings)
+    return instance.put<ResponseType>(`todo-lists/${todolistId}`, {title})
+  },
+  getTasks (todolistId: string) {
+return instance.get(`todo-lists/${todolistId}/tasks`)
+  },
+  createTask (todolistId: string, title: string) {
+    return instance.post(`todo-lists/${todolistId}/tasks`, {title})
+  },
+  deleteTask (todolistId: string, taskId: string) {
+    return instance.delete(`todo-lists/${todolistId}/tasks/${taskId}`)
+  },
+  updateTask (todolistId: string, taskId: string, title: string) {
+    return instance.put(`todo-lists/${todolistId}/tasks/${taskId}`, {title})
   }
 }
--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -52,3 +52,60 @@ export const UpdateTodolistTitle = () => {
   return <div>{JSON.stringify(state)}</div>
 }
 
+export const GetTasks = ()=> {
+  const [state, setState] = useState<any>(null)
+  useEffect(() => {
+    const id= 'ec4ec56f-8168-4ae6-928a-047e7870d610'
+    todolistsAPI.getTasks(id).then(response => {
+      setState(response.data)
+    })
+    // здесь мы будем делать запрос и ответ закидывать в стейт.
+    // который в виде строки будем отображать в div-ке
+
+  }, [])
+  return <div>{JSON.stringify(state)}</div>
+}
+
+export const CreateTask = ()=> {
+  const [state, setState] = useState<any>(null)
+  useEffect(() => {
+    const id= 'ec4ec56f-8168-4ae6-928a-047e7870d610'
+    const taskTitle = 'newTask'
+    todolistsAPI.createTask(id, taskTitle).then(response => {
+      setState(response.data)
+    })
+  }, [])
+  return <div>{JSON.stringify(state)}</div>
+}
+
+export const DeleteTasks = ()=> {
+  const [state, setState] = useState<any>(null)
+  useEffect(() => {
+    const todolistId= 'ec4ec56f-8168-4ae6-928a-047e7870d610'
+    const taskId= ''
+    todolistsAPI.deleteTask(todolistId, taskId).then(response => {
+      setState(response.data)
+    })
+    // здесь мы будем делать запрос и ответ закидывать в стейт.
+    // который в виде строки будем отображать в div-ке
+
+  }, [])
+  return <div>{JSON.stringify(state)}</div>
+}
+
+export const UpdateTasks = ()=> {
+  const [state, setState] = useState<any>(null)
+  useEffect(() => {
+    const todolistId= 'ec4ec56f-8168-4ae6-928a-047e7870d610'
+    const taskId= 'a2873742-b5da-473d-b35d-38532cae115d'
+    const taskTitle = 'changeTaskTitle'
+    todolistsAPI.updateTask(todolistId, taskId, taskTitle).then(response => {
+      setState(response.data)
+    })
+    // здесь мы будем делать запрос и ответ закидывать в стейт.
+    // который в виде строки будем отображать в div-ке
+
+  }, [])
+  return <div>{JSON.stringify(state)}</div>
+}
+
